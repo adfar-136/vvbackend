de11990435aa19a1d8504849a8f394ea0fb6806b
adfar-136 <adfarrasheed136@gmail.com>
2024-11-24 17:09:42 +0530

    done commit

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use("/discussions",DiscussionRouter)
   app.use(express.static(path.join(__dirname, '/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/builddd/index.html'));
-  });
+  })
 app.listen(process.env.PORT,()=>{
     console.log("Server is running on port 3000")
 })
